e0938cdce88d6cc363a49107c9593473aec83d05
JunichiroKohari <junichiro@kohari.jp>
2024-01-03 12:04:50 +0900

    Feat: add struct Tracer

--- tracer_test.go
+++ tracer_test.go
@@ -1,9 +1,18 @@
 package go_trace
 
 import (
+	"bytes"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
-	t.Errorf("todo")
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Errorf("returned value is nil")
+	}
+	tracer.Trace("hello")
+	if buf.String() != "hello" {
+		t.Errorf("output the wrong string: '%s'", buf.String())
+	}
 }
